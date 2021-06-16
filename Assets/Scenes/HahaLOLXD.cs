ba61e046dbce71dd5dcfdd73b2e58bdc1ace6c00
Splexxxer <Marcel.schlipf@gmx.de>
2021-06-16 23:06:49 +0200

    Script addet

---
+++ Assets/Scenes/HahaLOLXD.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HahaLOLXD : MonoBehaviour
+{
+    public bool hUANSOHN = true;
+    void Start()
+    {
+        if(hUANSOHN = true)
+        {
+            Debug.Log("Hi Bekir wenn du des liest dann gehts");
+        }
+                
+    }
+}
